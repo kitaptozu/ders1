270565fbdf70f70166c0b7ee6eb511e0aa19aa64
kitaptozu <mustafaalpcetin1@gmail.com>
2017-07-25 19:22:29 +0300

    Ders1

---
+++ src/tr/org/kamp/linux/firsApp/HelloWorld.java
@@ -0,0 +1,11 @@
+package tr.org.kamp.linux.firsApp;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+
+	}
+
+}
